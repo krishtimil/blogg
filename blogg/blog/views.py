ff2c6cbbe5bb28cc8bb793fbb572bccf527ac289
krishtimil <30183049+krishtimil@users.noreply.github.com>
2022-11-13 21:20:42 +0545

    feat: Added Blog Structure

---
+++ blogg/blog/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, get_object_or_404
+#from django.http import Http404
+
+from .models import Post
+# Create your views here.
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    # try:
+    #     post = Post.published.get(id=id)
+    # except Post.DoesNotExist:
+    #     raise Http404("No Post Found.")
+    
+    return render(request, 'blog/post/detail.html', {'post': post})
+    
+    
